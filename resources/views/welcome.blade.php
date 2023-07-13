25f35c115ccaa1b3f5cff7a82a88bbd51e1db964
Nivea Nyctra <113758640+niveanyctra@users.noreply.github.com>
2023-07-13 15:01:46 +0700

    update ui user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -19,7 +19,7 @@
                     <div class="col-12"><a href="{{ url('/dashboard') }}" class="btn btn-info">Dashboard</a>
                     </div>
                 @else
-                    <div class="col-6"><a href="{{ route('login') }}" class="btn btn-primary">Log
+                    <div class="col-12"><a href="{{ route('login') }}" class="btn btn-primary">Log
                             in</a></div>
 
                     @if (Route::has('register'))
@@ -39,7 +39,81 @@
         </div>
     </div>
     <div id="modal-action1" class="modal" tabindex="-1">
-
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title">Modal title</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col-4">
+                            <div class="mb-3">
+                                <label for="">Start Date :</label>
+                                <p class="start_date card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="mb-3">
+                                <label for="">End Date :</label>
+                                <p class="end_date card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-4">
+                            <div class="mb-3">
+                                <label for="">Jam :</label>
+                                <p class="jam card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="title">Kegiatan : </label>
+                                <p class="title card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="tempat">Tempat : </label>
+                                <p class="tempat card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="asal_surat">Asal Surat : </label>
+                                <p class="asal_surat card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="bidang_penanggung_jawab">Bidang Penanggung Jawab : </label>
+                                <p class="bpj card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="keterangan">Keterangan : </label>
+                                <p class="keterangan card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="no_surat">No Surat : </label>
+                                <p class="no_surat card py-2 px-2"></p>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="mb-3">
+                                <label for="no_agenda">No Agenda : </label>
+                                <p class="no_agenda card py-2 px-2"></p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                </div>
+            </div>
+        </div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
@@ -59,7 +133,29 @@
             themeSystem: 'bootstrap5',
             events: '{{ route('events.list') }}',
             editable: true,
-            eventClick: function ({ event }) {
+            dayMaxEventRows: true,
+            views: {
+                timeGrid: {
+                  dayMaxEventRows: 6 // adjust to 6 only for timeGridWeek/timeGridDay
+                }
+            },
+            eventClick: function(info){
+                const {event} = info;
+                console.log(event);
+                modal.modal('show');
+                modal.find('.modal-title').text('Agenda Kegiatan');
+                modal.find('.title').text(event.title);
+                modal.find('.start_date').text(event.startStr);
+                modal.find('.end_date').text(event.end.toISOString().substring(0, 10));
+                modal.find('.jam').text(event.extendedProps.jam);
+                modal.find('.bpj').text(event.extendedProps.bidang_penanggung_jawab);
+                modal.find('.tempat').text(event.extendedProps.tempat);
+                modal.find('.asal_surat').text(event.extendedProps.asal_surat);
+                modal.find('.no_surat').text(event.extendedProps.no_surat);
+                modal.find('.no_agenda').text(event.extendedProps.no_agenda);
+                modal.find('.keterangan').text(event.extendedProps.keterangan);
+            }
+            {{--  eventClick: function ({ event }) {
                 $.ajax({
                     url: `{{ url('events') }}/${event.id}/detail`,
                     success: function (res) {
@@ -83,7 +179,7 @@
                         })
                     }
                 })
-            },
+            },  --}}
             });
             calendar.render();
         });
