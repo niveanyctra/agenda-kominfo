69dd4c9af3abfe168b5804a0c94368195b65813d
RafRizu <thisrafi10@gmail.com>
2023-07-18 10:16:57 +0700

    export

--- resources/views/pages/laporan/cetak.blade.php
+++ resources/views/pages/laporan/cetak.blade.php
@@ -13,7 +13,7 @@
     <div>
         <p align="center"><b>Laporan Agenda Kegiatan</b></p>
         <p style="margin-left: 34px">Pada tanggal {{ $txtTglAwal }} s/d tanggal {{ $txtTglAkhir }}</p>
-        <table class="table table-bordered" align="center" rules="all" border="1px" style="width: 95%;">
+        <table class="table table-bordered" id="mauexport" align="center" rules="all" border="1px" style="width: 95%;">
             <thead>
                 <tr>
                     <th width="30px">ID</th>
@@ -48,9 +48,27 @@
             </tbody>
         </table>
     </div>
-    <script type="text/javascript">
-        window.print();
-    </script>
+<script>
+$(document).ready(function() {
+    $('#mauexport').DataTable( {
+        dom: 'Bfrtip',
+        buttons: [
+            'csv','excel', 'pdf'
+        ]
+    } );
+} );
+
+</script>
+
+<script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+<script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/dataTables.buttons.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.flash.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jszip/3.1.3/jszip.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/pdfmake.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.1.53/vfs_fonts.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.html5.min.js"></script>
+<script src="https://cdn.datatables.net/buttons/1.6.5/js/buttons.print.min.js"></script>
 </body>
 
 </html>
