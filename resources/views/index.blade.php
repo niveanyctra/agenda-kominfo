d192015dda1e946ae01aa7e6c7f6ec780ebc8227
RafRizu <thisrafi10@gmail.com>
2023-08-07 11:12:47 +0700

    footer

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -38,6 +38,28 @@
             </div>
         </div>
     </div>
+    <div class="row bg-primary">
+            <div class="col-4 ms-4">
+                <img class="ms-3 my-4" src="/adminlte/img/footerkominfo.png">
+
+                <div class="ms-3 fs-6 fw-light-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16">
+  <path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10zm0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6z"/>
+</svg>
+                    Alamat : Jl. Sunan Drajat No.15, Sumber,<Br> <div class="ms-3">Kec. Sumber,Cirebon, Jawa Barat 45611</div></div>
+                <div class="ms-3 mt-3 fs-6 fw-light-bold">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone" viewBox="0 0 16 16">
+  <path d="M3.654 1.328a.678.678 0 0 0-1.015-.063L1.605 2.3c-.483.484-.661 1.169-.45 1.77a17.568 17.568 0 0 0 4.168 6.608 17.569 17.569 0 0 0 6.608 4.168c.601.211 1.286.033 1.77-.45l1.034-1.034a.678.678 0 0 0-.063-1.015l-2.307-1.794a.678.678 0 0 0-.58-.122l-2.19.547a1.745 1.745 0 0 1-1.657-.459L5.482 8.062a1.745 1.745 0 0 1-.46-1.657l.548-2.19a.678.678 0 0 0-.122-.58L3.654 1.328zM1.884.511a1.745 1.745 0 0 1 2.612.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"/>
+</svg>
+                    Telepon : (0231) 8330580</div>
+            </div>
+            <div class="col-3"></div>
+            <div class="col-4">
+                <h5 class="ms-3 mt-4">PETA LOKASI</h5>
+                <iframe class="py-2 ms-3" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1175.0986719190741!2d108.47638729635923!3d-6.761425228544251!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6f1e52319b27c1%3A0xb2df85a13850e781!2sDiskominfo%20Kabupaten%20Cirebon!5e0!3m2!1sid!2sid!4v1691379479344!5m2!1sid!2sid" width="300" height="200"></iframe>
+            </div>
+
+    </div>
     <div id="modal-action1" class="modal" tabindex="-1">
         <div class="modal-dialog">
             <div class="modal-content">
